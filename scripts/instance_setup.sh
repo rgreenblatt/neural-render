8587a0ae0b0942168f90a37a8517af19a7a288f9
rgreenblatt <greenblattryan@gmail.com>
2020-05-21 10:44:36 -0400

    Added scikit-image

--- scripts/instance_setup.sh
+++ scripts/instance_setup.sh
@@ -2,4 +2,4 @@
 
 apt update && apt install -y git && 
   git clone https://github.com/rgreenblatt/neural-render &&
-  conda install -y pip && pip install pytorch-model-summary
+  conda install -y pip scikit-image && pip install pytorch-model-summary
