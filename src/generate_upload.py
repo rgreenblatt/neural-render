da712a5865d2ce7c2e060381f3aa47f37a2917b8
rgreenblatt <greenblattryan@gmail.com>
2020-06-14 16:03:32 -0400

    Script improvements

--- src/generate_upload.py
+++ src/generate_upload.py
@@ -62,11 +62,21 @@ if __name__ == "__main__":
 
     base_cmd = ('./scripts/blender_correct_py --background ' +
                 '--python src/generate.py')
-    seed = cfg.base_seed
+    seed_file = 'seed.out'
+    if os.path.exists(seed_file):
+        with open(seed_file, 'r') as f:
+            seed = int(f.read())
+    else:
+        seed = cfg.base_seed
+
     while True:
-        os.system("{} -- {} --seed {}".format(base_cmd,
-                                              cfg.base_as_arg_string(), seed))
         base_dir_name = 'generated'
+        shutil.rmtree(base_dir_name, ignore_errors=True)
+        return_code = os.system("{} -- {} --seed {}".format(
+            base_cmd, cfg.base_as_arg_string(), seed))
+        if return_code != 0:
+            print("blender command failed")
+            exit(return_code)
         dir_name = os.path.join(base_dir_name, 'seed_{}'.format(seed))
         shutil.make_archive(dir_name, 'zip', dir_name)
         shutil.rmtree(dir_name)
@@ -76,6 +86,8 @@ if __name__ == "__main__":
                 break
             except Exception as e:
                 print("FAILED UPLOAD with err:", e)
-        shutil.rmtree(base_dir_name)
 
         seed += 1
+
+        with open(seed_file, 'w') as f:
+            f.write(str(seed))
--- src/vast_ai_manager.py
+++ src/vast_ai_manager.py
@@ -1,17 +1,23 @@
 import subprocess
 import json
+import collections
+import time
 
 from generate_config import VastAIManagerConfig
 
-def get_offers(storage, *args):
-    out = subprocess.run([
-        'vast', 'search', 'offers', '--raw', '--storage',
-        str(storage), *args
-    ],
-                         capture_output=True)
+
+def json_vast_command(*args):
+    out = subprocess.run(['vast', *args], capture_output=True)
 
     return json.loads(out.stdout)
 
+
+def get_offers(storage, *args):
+    return json_vast_command('search', 'offers', '--raw', '--storage',
+                             str(storage), *args)
+
+def get_total_cost(offer, storage):
+    return offer['min_bid'] + offer['storage_total_cost']
 
 def sort_filter_offers(offers, storage, min_inet_up, min_inet_down,
                        usable_dl_perf, min_cuda):
@@ -22,27 +28,102 @@ def sort_filter_offers(offers, storage, min_inet_up, min_inet_down,
             continue
         if offer['cuda_max_good'] < min_cuda:
             continue
-        storage_cost_per_hour = offer['storage_cost'] / (24 * 30)
-        total_cost = offer['min_bid'] + storage_cost_per_hour * storage
+        total_cost = get_total_cost(offer, storage)
         effective_dl_perf = min(offer['dlperf'], usable_dl_perf)
-        dlperf_per_min_bid = effective_dl_perf / total_cost
-        print(dlperf_per_min_bid)
-        out_offers.append((dlperf_per_min_bid, offer))
+        dlperf_per_total_cost = effective_dl_perf / total_cost
+        out_offers.append((dlperf_per_total_cost, total_cost, effective_dl_perf, offer))
 
-    out_offers.sort(reverse=True)
+    out_offers.sort(key=lambda x: x[0], reverse=True)
 
     return out_offers
+
+def destroy(instance_id):
+    subprocess.run(['vast', 'destroy', 'instance', str(instance_id)])
+
+def get_instances():
+    return json_vast_command('show', 'instances', '--raw')
+
+InstanceInfo = collections.recordclass('InstanceInfo', ['dlperf_per_total_cost',
+                                      'total_cost', 'offer', 'start_time',
+                                      'seed'])
 
 def main():
     cfg = VastAIManagerConfig()
 
-    offers = sort_filter_offers(get_offers(cfg.storage, '--type=interruptible'),
-                       cfg.storage, cfg.min_inet_up, cfg.min_inet_down,
-                       cfg.usable_dl_perf, cfg.min_cuda)
+    instances = {}
+    instance_labels = []
+    label_counter = 0
 
-    print(offers)
+    while True:
+        actual_instances = get_instances()
+
+        for instance in actual_instances:
+            if (instance['label'] is not None
+                    and instance['label'] not in instance_labels):
+                print("unexpected instance:", instance['label'], "taking offline")
+                destroy(instance['id'])
+
+        all_offers = []
+        to_destroy_if_not_bid = []
+
+        for instance in actual_instances:
+            if (instance['next_state'] == 'stopped'
+                    or instance['intended_status'] == 'stopped'):
+                print("outbid instance:", instance['label'])
+
+                all_offers.append(instance)
+                to_destroy_if_not_bid.append(instance['label'])
+
+                instance_labels.remove(instance['label'])
+
+        all_offers.extend(get_offers(cfg.storage, '--type=interruptible'))
+
+        offers = sort_filter_offers(all_offers, cfg.storage, cfg.min_inet_up,
+                                    cfg.min_inet_down, cfg.usable_dl_perf,
+                                    cfg.min_cuda)
+
+        total_cost = sum(instances[label].total_cost
+                         for label in instance_labels)
+        sorted_instances = [(instances[label].dlperf_per_total_cost, label)
+                            for label in instance_labels]
+        sorted_instances.sort(key=lambda x: x[0])
+
+        additional = []
+
+        for (dlperf_per_total_cost, this_total_cost, effective_dl_perf,
+             offer) in offers:
+            if this_total_cost + total_cost < cfg.max_dl_per_hour:
+                total_cost += this_total_cost
+
+                is_existing = 'label' in offer
+                if is_existing:
+                    label = offer['label']
+                else:
+                    label = 'managed_instance_{}'.format(label_counter)
+                    label_counter += 1
+                instance_labels.append(label)
+                if is_existing:
+                    instances[label].total_cost = this_total_cost
+
+                instances.append(InstanceInfo(dlperf_per_total_cost,
+                                              this_total_cost,
+                                              start_time=
 
 
+        # def instance_start_time(i_id):
+        #     return time.time() -  wl
+
+        # replacable_instances = filter(lambda i_id: instances[i_id
+
+        # offers = sort_filter_offers(
+        #     get_offers(cfg.storage,
+        #                '--type=interruptible'), cfg.storage, cfg.min_inet_up,
+        #     cfg.min_inet_down, cfg.usable_dl_perf, cfg.min_cuda)
+
+        # for per, offer in offers:
+        #     print(per)
+
+        break
 
 
 if __name__ == "__main__":
